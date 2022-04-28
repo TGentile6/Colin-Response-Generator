f4353800dabd7254b91fbf70f0139eab29bb8aad
TGentile6 <91705356+TGentile6@users.noreply.github.com>
2022-04-28 01:15:39 -0400

    Added refresh button

--- main.js
+++ main.js
@@ -4,9 +4,25 @@ files[1] = "lmao.gif";
 files[2] = "Lol.gif";
 files[3] = "True-and-real.gif"
 
-rng = Math.floor(Math.random() * 4)
+const div = document.getElementById("container")
 
-const img = document.createElement("img");
-img.src = "Colin/" + files[rng];
-console.log(img.src);
-document.body.appendChild(img);
+function newImage(){
+    var rng = Math.floor(Math.random() * 4)
+    const img = document.createElement("img");
+    img.id = "image"
+    img.src = "Colin/" + files[rng];
+    div.appendChild(img);
+}
+
+function removeImage(){
+    const imgRef = document.getElementById("image")
+    imgRef.remove();
+}
+
+newImage()
+
+var button = document.getElementById('button');
+button.onclick = function(){
+    removeImage()
+    newImage()
+}
